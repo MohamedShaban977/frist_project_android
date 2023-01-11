ee89ced81b02046b5cc04d8fbcaaa8f7aeb9c0c9
Mohamd Shaban <shban977@gmail.com>
2023-01-11 23:00:49 +0200

    handle edit text password

--- app/src/main/java/com/example/startproject/LoginScreenActivity.kt
+++ app/src/main/java/com/example/startproject/LoginScreenActivity.kt
@@ -1,7 +1,9 @@
 package com.example.startproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
 import com.zeugmasolutions.localehelper.Locales
 import com.zeugmasolutions.localehelper.currentLocale
 import kotlinx.android.synthetic.main.activity_login_screen.*
@@ -10,14 +12,35 @@ class LoginScreenActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_screen)
+        changeLanguage()
+        handleDirectionPasswordEditText()
 
-but_lang.setOnClickListener{
-    if(currentLocale ==Locales.English) {
-        updateLocale(Locales.Arabic)
+
+
     }
-    else {
-        updateLocale(Locales.English)
+
+    private fun changeLanguage() {
+        but_lang.setOnClickListener {
+            if (currentLocale == Locales.English) {
+                updateLocale(Locales.Arabic)
+            } else {
+                updateLocale(Locales.English)
+            }
+        }
     }
-}
+
+    @SuppressLint("RtlHardcoded")
+    private fun handleDirectionPasswordEditText() {
+        if (currentLocale == Locales.Arabic) {
+            edtxt_password.gravity = Gravity.RIGHT or Gravity.CENTER_VERTICAL
+            edtxt_password.textDirection = View.TEXT_DIRECTION_RTL
+
+        } else {
+            edtxt_password.gravity = Gravity.LEFT or Gravity.CENTER_VERTICAL
+            edtxt_password.textDirection = View.TEXT_DIRECTION_LTR
+
+        }
     }
+
+
 }
